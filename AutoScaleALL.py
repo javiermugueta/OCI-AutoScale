b18871fe6e4f146799b49ce26a5e1a22c042803c
javi mugueta <javiermugueta@gmail.com>
2023-05-07 22:32:36 +0200

    Update AutoScaleALL.py
    .

--- AutoScaleALL.py
+++ AutoScaleALL.py
@@ -1812,4 +1812,4 @@ if cmd.dailyrepo:
                 else:
                     MakeLog("Error ({}) publishing notification - {}".format(ns_response.status, ns_response.message))
                     Retry = False
-#
+# 
